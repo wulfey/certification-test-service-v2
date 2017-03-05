b0b4759e71cc5d6a8c44513120c8c113388805b1
Jason Wolfe <jasonwolfe@gmail.com>
2017-03-05 00:50:05 +0000

    bootstrap

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,20 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_tenant!, :only => [ :index ]
+  # don't need to authenticate to view the index
+
+
 
   def index
+    # need to get tenant setting logic working
+    if current_user
+      if session[:tenant_id]
+        Tenant.set_current_tenant session[:tenant_id]
+      else 
+        Tenant.set_current_tenant current_user.tenants.first
+      end
+    @tenant = Tenant.current_tenant
+    params[:tenant_id] = @tenant.id
+    end
   end
+  
 end
